20e98432afb02262703a2bd4524e6a87f3145086
Teddy Wing <git@teddywing.com>
2020-08-02 10:23:52 +0200

    git-sugapply: Copy over new logic from `git-sugpatch`
    Use the new logic from `git-sugpatch` to build a `Config` and run
    `apply()` on all suggestion input arguments.

--- src/bin/git-sugapply.rs
+++ src/bin/git-sugapply.rs
@@ -1,25 +1,43 @@
 use std::env;
 use std::process;
 
-use github_suggestion::{Client, SuggestionUrl};
+use exitcode;
+
+use github_suggestion_cli::{gseprintln, for_suggestion};
+use github_suggestion_cli::config::Config;
 
 
 fn main() {
     let args: Vec<_> = env::args().collect();
 
-    if args.len() < 2 {
-        process::exit(111);
+    let config = match Config::get(
+        &args,
+        "usage: git sugapply [options] <suggestion>...",
+    ) {
+        Ok(c) => c,
+        Err(e) => {
+            gseprintln!(e);
+
+            process::exit(exitcode::CONFIG);
+        },
+    };
+
+    if config.suggestions.is_empty() {
+        config.print_usage();
+
+        process::exit(exitcode::USAGE);
     }
 
-    let url: SuggestionUrl = args[1].parse().unwrap();
-
-    let client = Client::new(
-        env!("GITHUB_TOKEN"),
-        &url.owner,
-        &url.repo,
-    ).unwrap();
-
-    let suggestion = client.fetch(&url.comment_id).unwrap();
-
-    suggestion.apply().unwrap();
+    for_suggestion(
+        &config,
+        |suggestion| {
+            match suggestion.apply() {
+                Err(e) => {
+                    gseprintln!(e);
+                    process::exit(exitcode::UNAVAILABLE);
+                },
+                _ => (),
+            }
+        },
+    );
 }
